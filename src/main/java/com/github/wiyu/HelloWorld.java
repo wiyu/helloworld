e133f6cf1264e63f8b24571a8e93e249a33d1801
William Yu <wyu@conductor.com>
2018-12-31 11:50:05 -0500

    initial commit with ci/cd test.

---
+++ src/main/java/com/github/wiyu/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.github.wiyu;
+
+public final class HelloWorld {
+
+    private HelloWorld() {
+    }
+
+    public static void main(final String[] args) {
+        System.out.println("Hello World");
+    }
+}
